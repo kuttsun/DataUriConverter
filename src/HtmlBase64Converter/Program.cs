7b74d491824aaad7dd5e47292c777894b4f4f6d3
kuttsun <tsukushimail+github@gmail.com>
2018-03-07 23:29:44 +0900

    VS プロジェクトの追加

---
+++ src/HtmlBase64Converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlBase64Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
